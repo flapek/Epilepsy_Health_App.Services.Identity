68dcf492b71ecdcc6f6d8ea812e7af3511b1fd67
flapek <filap4@gmail.com>
2020-08-22 11:05:13 +0200

    test jwt authentication

---
+++ src/Epilepsy_Health_App.Services.Identity.Api/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Epilepsy_Health_App.Services.Identity.Api.Controllers
+{
+    
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private IOptions<Audience> _settings;
+
+        public AuthController(IOptions<Audience> settings)
+        {
+            this._settings = settings;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string name, string pwd)
+        {
+            if (name == "catcher" && pwd == "123")
+            {
+
+                var now = DateTime.UtcNow;
+
+                var claims = new Claim[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, name),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, now.ToUniversalTime().ToString(), ClaimValueTypes.Integer64)
+                };
+
+                var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_settings.Value.Secret));
+
+                var jwt = new JwtSecurityToken(
+                    issuer: _settings.Value.Iss,
+                    audience: _settings.Value.Aud,
+                    claims: claims,
+                    notBefore: now,
+                    expires: now.Add(TimeSpan.FromMinutes(2)),
+                    signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+                );
+                var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+                var responseJson = new
+                {
+                    access_token = encodedJwt,
+                    expires_in = (int)TimeSpan.FromMinutes(2).TotalSeconds
+                };
+
+                return Json(responseJson);
+            }
+            else
+            {
+                return Json("");
+            }
+        }
+    }
+
+    public class Audience
+    {
+        public string Secret { get; set; }
+        public string Iss { get; set; }
+        public string Aud { get; set; }
+    }
+}
