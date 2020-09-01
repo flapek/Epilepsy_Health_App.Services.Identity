4f4203c7855c4422c363a04beea0a127df4dbd1c
flapek <filap4@gmail.com>
2020-09-01 20:56:39 +0200

    add new generic interface and method

---
+++ src/Epilepsy_Health_App.Services.Identity.Application/Commands/SignIn.cs
@@ -0,0 +1,12 @@
+﻿using Epilepsy_Health_App.Services.Identity.Application.DTO;
+using Joint.CQRS.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epilepsy_Health_App.Services.Identity.Application.Commands
+{
+    public class SignIn : ICommand<AuthDto>
+    {
+    }
+}
--- src/Epilepsy_Health_App.Services.Identity.Application/DTO/AuthDto.cs
+++ src/Epilepsy_Health_App.Services.Identity.Application/DTO/AuthDto.cs
@@ -1,10 +1,12 @@
+using Newtonsoft.Json;
+
 namespace Epilepsy_Health_App.Services.Identity.Application.DTO
 {
     public class AuthDto
     {
         public string AccessToken { get; set; }
+        [JsonIgnore]
         public string RefreshToken { get; set; }
-        public string Role { get; set; }
         public long Expires { get; set; }
     }
 }
